99be09d019413fba40d71dee901f31e06d0283e7
msyukri63-ui <msyukri63@gmail.com>
2026-05-08 21:22:16 +0800

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+from openai import OpenAI
 import feedparser
-import time
+
+client = OpenAI()
 
 feed = feedparser.parse("https://dpntimes.com/feed")
 
-for entry in feed.entries[:5]:
-    print(entry.title)
-    print(entry.link)
-    print("-" * 50)
+for entry in feed.entries[:1]:
 
-time.sleep(5)
+    prompt = f"Rewrite berita ini:\n{entry.title}"
+
+    response = client.chat.completions.create(
+        model="gpt-5-mini",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    print(response.choices[0].message.content)
