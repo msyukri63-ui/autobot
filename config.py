c7a26fa6530533b4e07f3884c0d64fb5ecb8642c
msyukri63-ui <msyukri63@gmail.com>
2026-05-09 00:06:44 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,23 @@
+GEMINI_API_KEYS = [
+    "API_KEY_1",
+    "API_KEY_2"
+]
 
+MODELS = [
+    "gemini-2.0-flash",
+    "gemini-2.5-flash"
+]
+
+RSS_FEED_URL = "https://dpntimes.com/feed"
+
+WORDPRESS_URL = "https://domainanda.com/wp-json/wp/v2/posts"
+
+WORDPRESS_MEDIA_URL = "https://domainanda.com/wp-json/wp/v2/media"
+
+WP_USERNAME = "admin"
+
+WP_APP_PASSWORD = "xxxx xxxx xxxx"
+
+POST_STATUS = "publish"
+
+DELAY_BETWEEN_POSTS = 60
